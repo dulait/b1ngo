0aac633bf700a976e6a614693ddb7ecdf939a62f
dulait <dd20211096@student.fon.bg.ac.rs>
2026-03-16 19:03:32 +0100

    feat: add domain event dispatcher

---
+++ server/src/B1ngo.Infrastructure/DomainEventDispatcher.cs
@@ -0,0 +1,28 @@
+using B1ngo.Domain.Core;
+
+namespace B1ngo.Infrastructure;
+
+public sealed class DomainEventDispatcher(IServiceProvider serviceProvider) : IDomainEventDispatcher
+{
+    public async Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default)
+    {
+        foreach (var domainEvent in domainEvents)
+        {
+            var handlerType = typeof(IDomainEventHandler<>).MakeGenericType(domainEvent.GetType());
+            var handlersEnumerableType = typeof(IEnumerable<>).MakeGenericType(handlerType);
+
+            var handlers = serviceProvider.GetService(handlersEnumerableType);
+
+            if (handlers is null)
+            {
+                continue;
+            }
+
+            foreach (var handler in (IEnumerable<object>)handlers)
+            {
+                var method = handlerType.GetMethod("HandleAsync");
+                await (Task)method!.Invoke(handler, [domainEvent, cancellationToken])!;
+            }
+        }
+    }
+}
