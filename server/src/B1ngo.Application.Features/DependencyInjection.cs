7e44d4973cb3710fca410d76cab6637137b204d2
dulait <dd20211096@student.fon.bg.ac.rs>
2026-03-16 19:03:14 +0100

    feat: add application DI registration

---
+++ server/src/B1ngo.Application.Features/DependencyInjection.cs
@@ -0,0 +1,38 @@
+using B1ngo.Application.Common;
+using B1ngo.Application.Features.Rooms.CreateRoom;
+using B1ngo.Application.Features.Rooms.EditSquare;
+using B1ngo.Application.Features.Rooms.EndGame;
+using B1ngo.Application.Features.Rooms.GetRoomState;
+using B1ngo.Application.Features.Rooms.JoinRoom;
+using B1ngo.Application.Features.Rooms.MarkSquare;
+using B1ngo.Application.Features.Rooms.Reconnect;
+using B1ngo.Application.Features.Rooms.StartGame;
+using B1ngo.Application.Features.Rooms.UnmarkSquare;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace B1ngo.Application.Features;
+
+public static class DependencyInjection
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddApplication()
+        {
+            services.AddValidatorsFromAssemblyContaining<CreateRoomCommandValidator>();
+
+            services.AddScoped<ICommandHandler<CreateRoomCommand, CreateRoomResponse>, CreateRoomHandler>();
+            services.AddScoped<ICommandHandler<JoinRoomCommand, JoinRoomResponse>, JoinRoomHandler>();
+            services.AddScoped<ICommandHandler<StartGameCommand, StartGameResponse>, StartGameHandler>();
+            services.AddScoped<ICommandHandler<EditSquareCommand, EditSquareResponse>, EditSquareHandler>();
+            services.AddScoped<ICommandHandler<MarkSquareCommand, MarkSquareResponse>, MarkSquareHandler>();
+            services.AddScoped<ICommandHandler<UnmarkSquareCommand, UnmarkSquareResponse>, UnmarkSquareHandler>();
+            services.AddScoped<ICommandHandler<EndGameCommand, EndGameResponse>, EndGameHandler>();
+
+            services.AddScoped<IQueryHandler<ReconnectQuery, ReconnectResponse>, ReconnectHandler>();
+            services.AddScoped<IQueryHandler<GetRoomStateQuery, GetRoomStateResponse>, GetRoomStateHandler>();
+
+            return services;
+        }
+    }
+}
