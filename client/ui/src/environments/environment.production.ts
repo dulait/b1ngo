d1e1ba9cc917a8fa6e6c2e8472eabe210746f372
dulait <dd20211096@student.fon.bg.ac.rs>
2026-03-18 19:43:29 +0100

    refactor: remove old client/ui angular app

--- client/ui/src/environments/environment.production.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://b1ngo-api-prod-production.up.railway.app',
-};
--- client/ui/src/environments/environment.staging.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'https://b1ngo-api-staging-production.up.railway.app',
-};
