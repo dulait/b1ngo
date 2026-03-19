1d9ebc7ddf81ec8ece93e6b4ad6e8bfe6f329263
dulait <dd20211096@student.fon.bg.ac.rs>
2026-03-19 20:40:17 +0100

    chore: delete empty UnitTest1.cs placeholder files

--- server/tests/B1ngo.Infrastructure.Tests/UnitTest1.cs
+++
@@ -1,7 +0,0 @@
-namespace B1ngo.Infrastructure.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1() { }
-}
--- server/tests/B1ngo.Web.Tests/UnitTest1.cs
+++
@@ -1,7 +0,0 @@
-namespace B1ngo.Web.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1() { }
-}
