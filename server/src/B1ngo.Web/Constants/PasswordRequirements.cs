0a48b710f54345f5464912a1cca3973495e735af
dulait <dd20211096@student.fon.bg.ac.rs>
2026-03-27 21:50:56 +0100

    feat(server): add PasswordRequirements constants and FluentValidation extension

---
+++ server/src/B1ngo.Web/Constants/PasswordRequirements.cs
@@ -0,0 +1,9 @@
+namespace B1ngo.Web.Constants;
+
+internal static class PasswordRequirements
+{
+    public const int MinimumLength = 8;
+    public const bool RequireDigit = true;
+    public const string DigitPattern = @"\d";
+    public const string DigitMessage = "Password must contain at least one digit.";
+}
---
+++ server/src/B1ngo.Web/Extensions/RuleBuilderExtensions.cs
@@ -0,0 +1,16 @@
+using B1ngo.Web.Constants;
+using FluentValidation;
+
+namespace B1ngo.Web.Extensions;
+
+internal static class RuleBuilderExtensions
+{
+    public static IRuleBuilderOptions<T, string> MeetsPasswordRequirements<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .NotEmpty()
+            .MinimumLength(PasswordRequirements.MinimumLength)
+            .Matches(PasswordRequirements.DigitPattern)
+            .WithMessage(PasswordRequirements.DigitMessage);
+    }
+}
